59d91fc105811436f01e8b3eef77da12f82d30c9
LeandroLeonCh <cleonc2@est.ups.edu.ec>
2021-04-29 21:46:52 -0500

    UNION JUEVES

--- Tesis/qr_flutter/lib/connections/urls.dart
+++ Tesis/qr_flutter/lib/connections/urls.dart
@@ -1,5 +1,5 @@
 class Conn {
-  static const String IP = '192.168.10.41'; //'192.168.18.4';'192.168.10.118'2
+  static const String IP = '192.168.18.4'; //'192.168.18.4';'192.168.10.118'2
   static const int PORT = 8080;
   static const String URL =
       'http://$IP:$PORT/operatingRoomRs/ws/operatingRoomServices';
